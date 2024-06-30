682021b92cd5c262881aac173e9e1624e54aae3d
xuexijiaoben <85658957+xuexijiaoben@users.noreply.github.com>
2024-06-30 14:25:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -57,6 +57,8 @@ function merge_package() {
 # git clone -b master --depth 1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/app/luci-app-unblockneteasemusic
 git clone --depth 1 https://github.com/ilxp/luci-app-ikoolproxy.git package/app/luci-app-ikoolproxy
 
+merge_package master https://github.com/coolsnowwolf/luci package/app applications/luci-app-easymesh
+
 ./scripts/feeds update -a
 
 merge_package openwrt-23.05 https://github.com/coolsnowwolf/luci feeds/luci/applications applications/luci-app-pppwn
